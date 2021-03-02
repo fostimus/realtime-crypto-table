53226bf64e9755b0d0ac589d64c21a2203ac6be6
Derek Foster <derekfoster94@gmail.com>
2021-03-01 17:17:17 -0800

    real time by calling API every second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useEffect, useState } from "react";
+import { useTable } from "react-table";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [renderToggle, setRenderToggle] = useState(false);
+  const [cryptoData, setCryptoData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then(response => {
+        setCryptoData(response.data);
+        setTimeout(() => setRenderToggle(!renderToggle), 1000);
+      });
+  }, [renderToggle]);
+
+  const data = useMemo(
+    () => [
+      {
+        col1: "Hello",
+        col2: "World"
+      },
+      {
+        col1: "react-table",
+        col2: "rocks"
+      },
+      {
+        col1: "whatever",
+        col2: "you want"
+      }
+    ],
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Column 1",
+        accessor: "col1" // accessor is the "key" in the data
+      },
+      {
+        Header: "Column 2",
+        accessor: "col2"
+      }
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({ columns, data });
+
+  console.log(cryptoData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // apply the table props
+
+    <>
+      <div>
+        {cryptoData.map(coin => (
+          <>
+            <div>Symbol: {coin.symbol}</div>
+            <div>Current Price: {coin.current_price}</div>
+            <div>Market Cap: {coin.market_cap}</div>
+          </>
+        ))}
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+          {// Loop over the header rows
+          headerGroups.map(headerGroup => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {// Loop over the headers in each row
+              headerGroup.headers.map(column => (
+                // Apply the header cell props
+                <th {...column.getHeaderProps()}>
+                  {// Render the header
+                  column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        {/* Apply the table body props */}
+        <tbody {...getTableBodyProps()}>
+          {// Loop over the table rows
+          rows.map(row => {
+            // Prepare the row for display
+            prepareRow(row);
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {// Loop over the rows cells
+                row.cells.map(cell => {
+                  // Apply the cell props
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {// Render the cell contents
+                      cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
