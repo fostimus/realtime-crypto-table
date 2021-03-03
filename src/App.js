fd35e97a31f8217fa6a8f5a029ab3a69a3717e6b
Derek Foster <derekfoster94@gmail.com>
2021-03-02 16:46:55 -0800

    text and padding styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { useMemo, useEffect, useState } from "react";
-import { useTable } from "react-table";
+import { useMemo, useEffect, useState, useRef } from "react";
+import { useTable, useSortBy } from "react-table";
 import axios from "axios";
 
 function App() {
   const [renderToggle, setRenderToggle] = useState(false);
+  const [coinCol, setCoinCol] = useState([]);
+  const [priceCol, setPriceCol] = useState([]);
+  const [athCol, setAthCol] = useState([]);
+  const [daysSinceAthCol, setDaysSinceAthCol] = useState([]);
+  const [marketCapCol, setMarketCapCol] = useState([]);
+  const [marketSharCol, setMarketShareCol] = useState([]);
+
   const [cryptoData, setCryptoData] = useState([]);
   // save as separate state for readability
   const [totalMarketShare, setTotalMarketShare] = useState(0);
+  const prevCryptoRef = useRef();
 
   const amountOfCoins = 100;
 
@@ -16,59 +24,71 @@ function App() {
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${amountOfCoins}&page=1&sparkline=false`
       )
       .then(response => {
-        console.log(response);
+        response.data.map((coin, index) => ({
+          col1: `${coin.name} (${coin.symbol})`,
+          col2: formatMoney(coin.current_price),
+          col3: formatMoney(coin.ath),
+          col4: daysSince(coin.ath_date),
+          col5: formatMoney(coin.market_cap),
+          col6: `${((coin.market_cap / totalMarketShare) * 100).toFixed(3)}%`
+        }));
         setCryptoData(response.data);
 
         setTotalMarketShare(
           response.data.reduce((acc, coin) => acc + coin.market_cap, 0)
         );
 
-        // re-render state to get updated data from coingecko
-        setTimeout(() => setRenderToggle(!renderToggle), 1000);
+        // re-render state to get updated data from coingecko, doing this render evrery second instead of infinitely
+        setTimeout(() => setRenderToggle(!renderToggle), 5000);
       });
+
+    // prevCryptoRef.current = cryptoData;
   }, [renderToggle]);
 
   // note: styling can only support up to 12 columns. to add more, need to add to tailwind.config.js
-  const coinFields = [
-    {
-      Header: "Coin (symbol)",
-      accessor: "col1"
-    },
-    {
-      Header: "Price",
-      accessor: "col2"
-    },
-    {
-      Header: "All Time High",
-      accessor: "col3"
-    },
-    {
-      Header: "Days Since ATH",
-      accessor: "col4"
-    },
-    {
-      Header: "Market Cap",
-      accessor: "col5"
-    },
-    {
-      Header: "Market Share",
-      accessor: "col6"
-    }
-  ];
+  const coinFields = useMemo(
+    () => [
+      {
+        Header: "Coin (symbol)",
+        accessor: "col1"
+      },
+      {
+        Header: "Price",
+        accessor: "col2"
+      },
+      {
+        Header: "All Time High",
+        accessor: "col3"
+      },
+      {
+        Header: "Days Since ATH",
+        accessor: "col4"
+      },
+      {
+        Header: "Market Cap",
+        accessor: "col5"
+      },
+      {
+        Header: "Market Share",
+        accessor: "col6"
+      }
+    ],
+    []
+  );
 
-  const columns = useMemo(() => coinFields, []);
+  const columns = useMemo(() => coinFields, [coinFields]);
 
   const data = useMemo(
     () =>
-      cryptoData.map(coin => ({
+      cryptoData.map((coin, index) => ({
         col1: `${coin.name} (${coin.symbol})`,
-        col2: coin.current_price,
-        col3: `$${new Intl.NumberFormat().format(coin.ath)}`,
+        col2: formatMoney(coin.current_price),
+        col3: formatMoney(coin.ath),
         col4: daysSince(coin.ath_date),
-        col5: `$${new Intl.NumberFormat().format(coin.market_cap)}`,
+        col5: formatMoney(coin.market_cap),
         col6: `${((coin.market_cap / totalMarketShare) * 100).toFixed(3)}%`
       })),
-    [cryptoData, totalMarketShare]
+    [totalMarketShare, cryptoData]
   );
 
   const {
@@ -77,14 +97,18 @@ function App() {
     headerGroups,
     rows,
     prepareRow
-  } = useTable({ columns, data });
+  } = useTable({ columns, data }, useSortBy);
 
-  const tdStyles = `w-1/${coinFields.length}`;
+  /****** styles ******/
+  const tdStyles = `py-4 px-14 md:px-4 w-1/${coinFields.length}`;
   const trStyles = `border-b border-grey-300`;
 
   return (
     <>
-      <table className="mx-auto text-center" {...getTableProps()}>
+      <table
+        className="mx-auto text-center text-sm md:text-base"
+        {...getTableProps()}
+      >
         <thead>
           {// Loop over the header rows
           headerGroups.map(headerGroup => (
@@ -93,9 +117,19 @@ function App() {
               {// Loop over the headers in each row
               headerGroup.headers.map(column => (
                 // Apply the header cell props
-                <th className={`${tdStyles} h-12`} {...column.getHeaderProps()}>
+                <th
+                  className={`${tdStyles} h-12`}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
                   {// Render the header
                   column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
                 </th>
               ))}
             </tr>
@@ -146,4 +180,20 @@ function daysSince(date) {
   let diff = Date.now() - date;
 
   return Math.round(diff / ONE_DAY);
+}
+
+function formatMoney(amount) {
+  let formattedAmount = `$${new Intl.NumberFormat().format(
+    Math.round(amount * 100) / 100
+  )}`;
+
+  // if decimal and only one digit after decimal
+  if (
+    formattedAmount.includes(".") &&
+    formattedAmount.indexOf(".") === formattedAmount.length - 2
+  ) {
+    formattedAmount = formattedAmount + "0";
+  }
+
+  return formattedAmount;
 }
